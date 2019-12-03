9002d9fe61cfec9abdc08296964583e677f4626b
FORSBERG+two <bjorn@forsbergplustwo.com>
2019-12-03 12:17:54 +0100

    Refactor stimulus application controller to use initialize instead of connect for setting up the app bridge

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -9,21 +9,39 @@
 
 import { Controller } from 'stimulus'
 import createApp from '@shopify/app-bridge'
-import { Toast } from '@shopify/app-bridge/actions'
+import { Toast, History, Loading } from '@shopify/app-bridge/actions'
 
 export default class extends Controller {
   initialize () {
     this.flashDuration = 5000
-  }
-
-  connect () {
+    // Create the App Bridge
     const data = document.getElementById('shopify-app-init').dataset
-    const createdApp = createApp({
+    const app = createApp({
       apiKey: data.apiKey,
       shopOrigin: data.shopOrigin,
       forceRedirect: true
     })
-    this.appBridge = createdApp
+    this.appBridge = app
+
+    // Create the App Bridge History Helper
+    const history = History.create(app)
+    this.appBridgeHistory = history
+
+    // Create the App Bridge Loading Indicator
+    const loading = Loading.create(app)
+    this.appBridgeLoader = loading
+  }
+
+  connect () {
+    this.updateHistory()
+    setTimeout(() => {
+      this.stopLoading()
+    }, 300)
+    console.log('Connected!')
+  }
+
+  disconnect () {
+    this.startLoading()
   }
 
   flashNotice (message) {
@@ -44,5 +62,17 @@ export default class extends Controller {
     }
     const toastNotice = Toast.create(this.appBridge, options)
     toastNotice.dispatch(Toast.Action.SHOW)
+  }
+
+  updateHistory () {
+    this.appBridgeHistory.dispatch(History.Action.PUSH, window.location.pathname)
+  }
+
+  startLoading () {
+    this.appBridgeLoader.dispatch(Loading.Action.START)
+  }
+
+  stopLoading () {
+    this.appBridgeLoader.dispatch(Loading.Action.STOP)
   }
 }
